cdc7492d44f177a3a086668c4c7bf4caff100ef7
Wesley Ruedebusch <w.ruedebusch@gmail.com>
2020-07-10 12:33:42 -0400

    botyh OOps done i think OOP2 has a testing error

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle - base
+class Vehicle:
+    pass
+
+# Flight vehicle - child of vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Stareship child of the flight vehicle
+class Starship(FlightVehicle):
+    pass
+
+# Airplane child of FlightVehicle
+class Airplane(FlightVehicle):
+    pass
+
+# GroundVehicle child of vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+# Car child of GroundVehicle
+class Car(GroundVehicle):
+    pass
+
+# Motorcycle child of GroundVehicle
+class Motorcycle(GroundVehicle):
+    pass
--- src/oop/test_oop2.py
+++ src/oop/test_oop2.py
@@ -12,13 +12,13 @@ class Oop2Tests(unittest.TestCase):
   def test_ground_vehicle_num_wheels(self):
     self.assertEqual(self.ground_vehicle.num_wheels, 4)
 
-  def test_motocycle_num_wheels(self):
+  def test_motorcycle_num_wheels(self):
     self.assertEqual(self.motorcycle.num_wheels, 2)
 
   def test_ground_vehicle_drive(self):
     self.assertEqual(self.ground_vehicle.drive(), "vroooom")
 
-  def test_motorcyle_drive(self):
+  def test_motorcycle_drive(self):
     self.assertEqual(self.motorcycle.drive(), "BRAAAP!!")
 
 
